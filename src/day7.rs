538fd2827018a591b374befc241b8cc2ce31cbf6
Geir Olav Alsvik <goa95@protonmail.com>
2021-12-07 20:53:43 +0100

    simplify code

--- src/day7.rs
+++ src/day7.rs
@@ -1,25 +1,14 @@
+use itertools::Itertools;
+
 pub fn part_one(input: &str) -> i32 {
-    let mut numbers = into_i32_vec(input);
-    part_1_median(&mut numbers)
-}
-
-pub fn part_two(input: &str) -> i32 {
-    let numbers = into_i32_vec(input);
-    part_2_average_floored(&numbers)
-}
-
-fn into_i32_vec(input: &str) -> Vec<i32> {
-    input.split(',').map(|n| n.parse().unwrap()).collect()
-}
-
-fn part_1_median(numbers: &mut [i32]) -> i32 {
-    numbers.sort();
+    let numbers: Vec<i32> = input.split(',').map(|n| n.parse().unwrap()).sorted().collect();
     let median = numbers[numbers.len() / 2];
     numbers.iter().map(|&n| (n - median).abs()).sum()
 }
 
 // Might be off by one
-fn part_2_average_floored(numbers: &[i32]) -> i32 {
+pub fn part_two(input: &str) -> i32 {
+    let numbers: Vec<i32> = input.split(',').map(|n| n.parse().unwrap()).collect();
     let average = numbers.iter().sum::<i32>() / numbers.len() as i32;
     numbers.iter().map(|&n| calc_fuel_part2(n, average)).sum()
 }
