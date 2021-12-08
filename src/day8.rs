a39134fddbda99130629a9db2ac363b3013a0fa1
Geir Olav Alsvik <goa95@protonmail.com>
2021-12-08 17:00:45 +0100

    day 8 part 1 one done

---
+++ src/day8.rs
@@ -0,0 +1,16 @@
+pub fn part_one(input: &str) -> usize {
+    input
+        .lines()
+        .flat_map(|it| it.split(" | ").nth(1).unwrap().split(' '))
+        .filter(|it| [2, 4, 3, 7].contains(&it.len()))
+        .count()
+}
+
+pub fn part_two(input: &str) -> i64 {
+    0
+}
+
+#[test]
+fn example_part1() {
+    assert_eq!(part_one(include_str!("day8sample.input")), 26)
+}
