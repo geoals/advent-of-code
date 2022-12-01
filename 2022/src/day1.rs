34918c0857d3678f15aeb7fdd783f5412ffd127a
Geir Olav Alsvik <geir.olav.alsvik@sparebank1.no>
2022-12-01 16:12:03 +0100

    use sorted() and rev() from itertools for day 1

--- 2022/src/day1.rs
+++ 2022/src/day1.rs
@@ -1,12 +1,11 @@
+use itertools::Itertools;
+
 pub fn part_one(input: &str) -> i32 {
     calories_sum(input).into_iter().max().unwrap()
 }
 
 pub fn part_two(input: &str) -> i32 {
-    let mut calories = calories_sum(input);
-    calories.sort();
-    calories.reverse();
-    calories.iter().take(3).sum()
+    calories_sum(input).iter().take(3).sum()
 }
 
 fn calories_sum(input: &str) -> Vec<i32> {
@@ -15,6 +14,8 @@ fn calories_sum(input: &str) -> Vec<i32> {
             .split('\n')
             .map(|x| x.parse::<i32>().unwrap()).sum()
         )
+        .sorted()
+        .rev()
         .collect()
 }
 
