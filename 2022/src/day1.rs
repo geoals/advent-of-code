53a29b4dde7150af0b4372ba2c4fbf3add292028
Geir Olav Alsvik <geir.olav.alsvik@sparebank1.no>
2022-12-01 15:08:50 +0100

    day 1 done

--- 2022/src/day1.rs
+++ 2022/src/day1.rs
@@ -1,19 +1,31 @@
 pub fn part_one(input: &str) -> i32 {
-    1
+    calories_sum(input).into_iter().max().unwrap()
 }
 
 pub fn part_two(input: &str) -> i32 {
-    1
+    let mut calories = calories_sum(input);
+    calories.sort();
+    calories.reverse();
+    calories.iter().take(3).sum()
+}
+
+fn calories_sum(input: &str) -> Vec<i32> {
+    input.split("\n\n")
+        .map(|a| a.trim()
+            .split('\n')
+            .map(|x| x.parse::<i32>().unwrap()).sum()
+        )
+        .collect()
 }
 
 #[test]
 fn example_input_part1() {
-    let input = include_str!("../input/sample_01.txt");
-    assert_eq!(part_one(input), 10000);
+    let input = include_str!("../input/sample_1.txt");
+    assert_eq!(part_one(input), 24000);
 }
 
 #[test]
 fn example_input_part2() {
-    let input = include_str!("../input/sample_01.txt");
-    assert_eq!(part_two(input), 5);
+    let input = include_str!("../input/sample_1.txt");
+    assert_eq!(part_two(input), 45000);
 }
