eaff994dfeb2d7cd3e00cb2351e537fbf71680ef
Geir Olav Alsvik <geir.olav.alsvik@sparebank1.no>
2022-12-08 16:36:47 +0100

    day 8 cleanup

--- 2022/src/day8.rs
+++ 2022/src/day8.rs
@@ -1,9 +1,9 @@
 pub fn part_one(input: &str) -> usize {
     let mut num_visible = 0;
     let all_rows = input.lines().collect::<Vec<&str>>();
-    for (i_row, row) in all_rows.iter().enumerate() {
-        for (i_col, col) in row.chars().enumerate() {
-            num_visible += is_visible(i_row, row, i_col, &all_rows) as usize;
+    for (y, row) in all_rows.iter().enumerate() {
+        for (x, _) in row.chars().enumerate() {
+            num_visible += is_visible(y, row, x, &all_rows) as usize;
         }
     }
     num_visible
@@ -12,51 +12,48 @@ pub fn part_one(input: &str) -> usize {
 pub fn part_two(input: &str) -> usize {
     let mut scenic_scores = vec![];
     let all_rows = input.lines().collect::<Vec<&str>>();
-    for (i_row, row) in all_rows.iter().enumerate() {
-        for (i_col, col) in row.chars().enumerate() {
-            scenic_scores.push(scenic_score(i_row, row, i_col, &all_rows))
+    for (y, row) in all_rows.iter().enumerate() {
+        for (x, _) in row.chars().enumerate() {
+            scenic_scores.push(scenic_score(y, row, x, &all_rows))
         }
     }
     *scenic_scores.iter().max().unwrap()
 }
 
 fn is_visible(y: usize, row: &str, x: usize, all_rows: &Vec<&str>) -> bool {
-    if y == 0 || y == all_rows.len() - 1 || x == 0 || x == row.len() - 1 {
-        return true
-    }
     let row_heights = row.chars().map(|x| x.to_digit(10).unwrap()).collect::<Vec<u32>>();
+    let col_heights = column_vec_from_rows(x, all_rows);
     let current_height = row_heights[x];
-    let mut col_heights = vec![];
-    for i in 0..all_rows.len() {
-        let height = (all_rows[i].as_bytes()[x] as char).to_digit(10).unwrap();
-        col_heights.push(height);
-    }
-    if row_heights[0..x].iter().filter(|tree_height| **tree_height >= current_height).count() == 0 {
-        return true;
-    }
-    if row_heights[x+1..].iter().filter(|tree_height| **tree_height >= current_height).count() == 0 {
-        return true;
-    }
-    if col_heights[0..y].iter().filter(|tree_height| **tree_height >= current_height).count() == 0 {
-        return true;
-    }
-    if col_heights[y+1..].iter().filter(|tree_height| **tree_height >= current_height).count() == 0 {
+
+    visible_from_direction(current_height, &row_heights[0..x])
+        || visible_from_direction(current_height, &row_heights[x + 1..])
+        || visible_from_direction(current_height, &col_heights[0..y])
+        || visible_from_direction(current_height, &col_heights[y + 1..])
+}
+
+fn visible_from_direction(current_height: u32, it: &[u32]) -> bool {
+    if it.iter().filter(|tree_height| **tree_height >= current_height).count() == 0 {
         return true;
     }
     false
 }
 
-fn scenic_score(y: usize, row: &str, x: usize, all_rows: &Vec<&str>) -> usize {
-    let row_heights = row.chars().map(|x| x.to_digit(10).unwrap()).collect::<Vec<u32>>();
-    let current_height = row_heights[x];
+fn column_vec_from_rows(x: usize, all_rows: &Vec<&str>) -> Vec<u32> {
     let mut col_heights = vec![];
     for i in 0..all_rows.len() {
         let height = (all_rows[i].as_bytes()[x] as char).to_digit(10).unwrap();
         col_heights.push(height);
     }
+    col_heights
+}
 
-    let num_right = score_direction(current_height, &row_heights, (x+1..row_heights.len()).collect());
-    let num_down = score_direction(current_height, &col_heights, (y+1..col_heights.len()).collect());
+fn scenic_score(y: usize, row: &str, x: usize, all_rows: &Vec<&str>) -> usize {
+    let row_heights = row.chars().map(|x| x.to_digit(10).unwrap()).collect::<Vec<u32>>();
+    let col_heights = column_vec_from_rows(x, all_rows);
+    let current_height = row_heights[x];
+
+    let num_right = score_direction(current_height, &row_heights, (x + 1..row_heights.len()).collect());
+    let num_down = score_direction(current_height, &col_heights, (y + 1..col_heights.len()).collect());
     let num_left = score_direction(current_height, &row_heights, (0..x).rev().collect());
     let num_up = score_direction(current_height, &col_heights, (0..y).rev().collect());
 
