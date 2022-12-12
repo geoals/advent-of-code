7966db1c49db6f1840010996be90c6b13068a062
Geir Olav Alsvik <goa95@protonmail.com>
2022-12-12 20:58:52 +0100

    day 9 part 2 and cleanup

--- 2022/src/day9.rs
+++ 2022/src/day9.rs
@@ -1,112 +1,60 @@
 use std::collections::{HashSet, HashMap};
 
 pub fn part_one(input: &str) -> usize {
-    let motions = input.lines().map(|line| {
-        let (direction, count ) = line.split_once(' ').unwrap();
-        (direction, count.parse::<i32>().unwrap())
-    });
-
-    let mut previous_head_pos;
-    let mut head_pos = (0, 0);
-    let mut tail_pos = (0, 0);
-    let mut tail_visited = HashSet::<(i32, i32)>::from([tail_pos]);
-
-    for (direction, count) in motions {
-        for _ in 0..count {
-            previous_head_pos = head_pos;
-            match direction {
-                "R" => head_pos.0 += 1,
-                "D" => head_pos.1 -= 1,
-                "L" => head_pos.0 -= 1,
-                "U" => head_pos.1 += 1,
-                _ => panic!()
-            }
-            if !is_adjacent(head_pos, tail_pos) { 
-                tail_pos = previous_head_pos; 
-                tail_visited.insert(tail_pos);
-            }
-        }
-    }
-
-    tail_visited.len()
-}
-
-fn is_adjacent(a: (i32, i32), b: (i32, i32)) -> bool {
-    ((b.0 - a.0).pow(2) as f32 + (b.1 - a.1).pow(2) as f32).sqrt() <= 2_f32.sqrt()
+    run(input, 2)
 }
 
 pub fn part_two(input: &str) -> usize {
+    run(input, 10)
+}
+
+fn run(input: &str, num_knots: usize) -> usize {
     let motions = input.lines().map(|line| {
         let (direction, count ) = line.split_once(' ').unwrap();
         (direction, count.parse::<i32>().unwrap())
     });
-
     let mut tail_visited = HashSet::<(i32, i32)>::from([(0, 0)]);
-    let mut knot_positions: HashMap<i32, (i32, i32)>= (0..10).map(|knot| {
-        (knot, (0, 0))
-    }).collect();
-
+    let mut knot_positions = vec![(0, 0); num_knots];
 
     for (direction, count) in motions {
-
         for _ in 0..count {
-            let mut previous_head_pos = (0, 0);
-
-            for knot in 0..10 {
-                let inc = if knot != 0 { 1 } else { 0 };
-                let mut head_pos = knot_positions[&(knot - inc)];
-                let mut tail_pos = knot_positions[&knot];
-
-                // special handling for head: 
+            for knot in 0..num_knots {
                 if knot == 0 {
-                    let prev = head_pos;
-
                     match direction {
-                        "R" => head_pos.0 += 1,
-                        "D" => head_pos.1 -= 1,
-                        "L" => head_pos.0 -= 1,
-                        "U" => head_pos.1 += 1,
+                        "R" => knot_positions[knot].0 += 1,
+                        "D" => knot_positions[knot].1 -= 1,
+                        "L" => knot_positions[knot].0 -= 1,
+                        "U" => knot_positions[knot].1 += 1,
                         _ => panic!()
                     }
-                    knot_positions.insert(knot, head_pos);
-                    println!("Moved knot {:?} from {:?} to {:?} ", knot, prev, head_pos);
-                } else {
-                    if is_adjacent(head_pos, tail_pos) {
-                        continue
+                    continue;
+                } 
+                let next_knot_pos = knot_positions[knot - 1];
+
+                if !is_adjacent(next_knot_pos, knot_positions[knot]) {
+                    let direction_vector = get_direction(knot_positions[knot], next_knot_pos);
+                    knot_positions[knot].0 += direction_vector.0;
+                    knot_positions[knot].1 += direction_vector.1;
+                    if knot == num_knots - 1 {
+                        tail_visited.insert(knot_positions[knot]);
                     }
-                    let prev = tail_pos;
-                    if !is_adjacent(head_pos, tail_pos) && (head_pos.0 == tail_pos.0 || head_pos.1 == tail_pos.1) { // same row or col
-                        match direction {
-                            "R" => tail_pos.0 += 1,
-                            "D" => tail_pos.1 -= 1,
-                            "L" => tail_pos.0 -= 1,
-                            "U" => tail_pos.1 += 1,
-                            _ => panic!()
-                        }
-                        knot_positions.insert(knot, tail_pos);
-                        println!("Moved knot {:?} from {:?} to {:?} ", knot, prev, tail_pos);
-                    } else {
-                        // move diagonal in direction of knotnr knot-1
-                        println!("knot {} at {:?} should go diagonal now towards {:?}", knot, tail_pos, head_pos);
-                    }
-                    // let temp = previous_head_pos;
-                    // previous_head_pos = tail_pos;
-                    // println!("head {:?} tail {:?} knot {}", head_pos, tail_pos, knot);
-                    // println!("Finding next position for knot[{}], currently at {:?}", knot, tail_pos);
-                    // tail_pos = temp;
-                    // knot_positions.insert(knot, tail_pos);
-                    // println!("Moved knot {:?} from {:?} to {:?} ", knot, previous_head_pos, temp);
-                    // if !is_adjacent(head_pos, tail_pos) { 
-                    //     // tail_pos = previous_head_pos; 
-                    //     tail_visited.insert(tail_pos);
-                    // }
-                    // println!("{:?}", knot_positions);
                 }
             }
         }
     }
 
     tail_visited.len()
+}
+
+fn get_direction(a: (i32, i32), b: (i32, i32)) -> (i32, i32) {
+    let vector = (b.0 - a.0, b.1 - a.1);
+    let x = if vector.0 == 0 { 0 } else if vector.0 > 0 { 1 } else  { -1 };
+    let y = if vector.1 == 0 { 0 } else if vector.1 > 0 { 1 } else  { -1 };
+    (x, y)
+}
+
+fn is_adjacent(a: (i32, i32), b: (i32, i32)) -> bool {
+    ((b.0 - a.0).pow(2) as f32 + (b.1 - a.1).pow(2) as f32).sqrt() <= 2_f32.sqrt()
 }
 
 #[test]
@@ -126,14 +74,15 @@ R 2";
 #[test]
 fn example_input_part2() {
     let input = "\
-R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2";
-    assert_eq!(part_two(input), 13);
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+";
+    assert_eq!(part_two(input), 36);
 }
 
