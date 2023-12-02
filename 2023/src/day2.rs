ceb27b2f2d8b1235300f28a16ea975466cc86888
Geir Olav Alsvik <goa95@protonmail.com>
2023-12-02 13:16:38 +0100

    day 2 part 2 done

--- 2023/src/day2.rs
+++ 2023/src/day2.rs
@@ -36,16 +36,43 @@ fn to_game_id((game_num, _): (&str, &str)) -> i32 {
 
 
 pub fn part_two(input: &str) -> i32 {
-    0
+    input.lines()
+        .map(power)
+        .sum()
 }
 
-#[test]
-fn example_input_part1() {
-    let input = r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+fn power(game: &str) -> i32 {
+    let game = game.split_once(": ").unwrap().1;
+    let draws = game.split(|c| c == ',' || c == ';').collect();
+
+    let blue = min_amount_required_of_color("blue", &draws);
+    let green = min_amount_required_of_color("green", &draws);
+    let red = min_amount_required_of_color("red", &draws);
+
+    blue * green * red
+}
+
+fn min_amount_required_of_color(color: &str, draws: &Vec<&str>) -> i32 {
+    draws
+        .iter()
+        .filter(|draw| draw.ends_with(color))
+        .map(|draw| draw.trim().split_once(' ').unwrap().0.parse::<i32>().unwrap())
+        .max().unwrap()
+}
+
+const sample_input: &str = r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 "#;
-    assert_eq!(part_one(input), 8);
+
+#[test]
+fn example_input_part1() {
+    assert_eq!(part_one(sample_input), 8);
+}
+
+#[test]
+fn example_input_part2() {
+    assert_eq!(part_two(sample_input), 2286);
 }
