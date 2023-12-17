3ec0d748c4cb535637c509ec02f10ba3a76b5078
Geir Olav Alsvik <geir.olav.alsvik@sparebank1.no>
2023-12-17 16:30:59 +0100

    day 11 part 2 done

--- 2023/src/day11.rs
+++ 2023/src/day11.rs
@@ -1,57 +1,71 @@
-pub fn part_one(input: &str) -> i32 {
-    let universe = expand_universe(input);
-    let galaxies = get_galaxy_positions(&universe);
-    galaxies.iter().map(|galaxy| {
-        galaxies.iter().map(|other_galaxy| distance(*galaxy, *other_galaxy))
-            .sum::<i32>()
-    }).sum::<i32>() / 2
+pub fn part_one(input: &str) -> i64 {
+    sum_distances(input, 2)
 }
 
-fn expand_universe(input: &str) -> Vec<Vec<char>> {
+pub fn part_two(input: &str) -> i64 {
+    sum_distances(input, 1_000_000)
+}
+
+fn sum_distances(input: &str, multiplier: i64) -> i64 {
     let universe = input.lines()
         .map(|row| row.chars().collect())
         .collect::<Vec<Vec<char>>>();
-    expand_rows(transpose(expand_rows(universe)))
+    let galaxies = get_galaxy_positions(&universe, multiplier);
+
+    galaxies.iter().map(|galaxy| {
+        galaxies.iter().map(|other_galaxy| distance(*galaxy, *other_galaxy))
+            .sum::<i64>()
+    }).sum::<i64>() / 2
 }
 
-fn expand_rows(rows: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let mut new_rows = Vec::new();
-    for row in &rows {
-        new_rows.push(row.clone());
-        if row.iter().all(|c| *c == '.') {
-            new_rows.push(row.clone());
-        }
-    }
-    new_rows
+fn distance(a: (i64, i64), b: (i64, i64)) -> i64 {
+    (a.0 - b.0).abs() + (a.1 - b.1).abs()
 }
 
-fn transpose(rows: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    (0..rows[0].len())
-        .map(|col| {
-            (0..rows.len())
-                .map(|row| rows[row][col])
-                .collect()
-        }).collect()
-}
+fn get_galaxy_positions(universe: &[Vec<char>], multiplier: i64) -> Vec<(i64, i64)> {
+    let empty_rows = get_empty_rows(universe);
+    let empty_cols = get_empty_columns(universe);
 
-fn get_galaxy_positions(universe: &[Vec<char>]) -> Vec<(i32, i32)> {
     let mut positions = Vec::new();
     for (row, line) in universe.iter().enumerate() {
         for (col, c) in line.iter().enumerate() {
-            if *c == '#' {
-                positions.push((row as i32, col as i32));
-            }
+            if *c != '#' { continue; }
+            positions.push(
+                (row as i64 + (empty_rows[row] * (multiplier - 1)),
+                 col as i64 + (empty_cols[col] * (multiplier - 1)))
+            );
         }
     }
     positions
 }
 
-fn distance(a: (i32, i32), b: (i32, i32)) -> i32 {
-    (a.0 - b.0).abs() + (a.1 - b.1).abs()
+fn get_empty_rows(universe: &[Vec<char>]) -> Vec<i64> {
+    let mut is_empty = vec![true; universe.len()];
+    for (row, line) in universe.iter().enumerate() {
+        for &cell in line {
+            if cell == '#' { is_empty[row] = false; }
+        }
+    }
+    cumulative_counts(is_empty)
 }
 
-pub fn part_two(input: &str) -> i32 {
-    0
+
+fn get_empty_columns(universe: &[Vec<char>]) -> Vec<i64> {
+    let mut is_empty = vec![true; universe[0].len()];
+    for row in universe {
+        for (col, &cell) in row.iter().enumerate() {
+            if cell == '#' { is_empty[col] = false; }
+        }
+    }
+    cumulative_counts(is_empty)
+}
+
+/// Collect list of bools to cumulative counts, e.g. [true, false, true] -> [1, 1, 2]
+fn cumulative_counts(is_empty: Vec<bool>) -> Vec<i64> {
+    is_empty.iter().scan(0, |acc, &x| {
+        *acc += usize::from(x) as i64;
+        Some(*acc)
+    }).collect()
 }
 
 mod tests {
@@ -59,7 +73,8 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let input = "...#......
+        let input = "\
+...#......
 .......#..
 #.........
 ..........
@@ -71,21 +86,5 @@ mod tests {
 #...#.....";
 
         assert_eq!(part_one(input), 374);
-    }
-
-    #[test]
-    fn test_part_two() {
-        let input = "...#......
-.......#..
-#.........
-..........
-......#...
-.#........
-.........#
-..........
-.......#..
-#...#.....";
-
-        assert_eq!(part_two(input), 0);
     }
 }
