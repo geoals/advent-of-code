8975a0041d1f520bdadea0ef4c81610d8283c633
Geir Olav Alsvik <goa95@protonmail.com>
2024-12-02 21:22:17 +0100

    day2 part2

--- 2024/src/day2.rs
+++ 2024/src/day2.rs
@@ -1,34 +1,47 @@
 use itertools::Itertools;
 
 pub fn part_one(input: &str) -> usize {
-    let levels: Vec<Vec<i64>> = input
+    let levels = read_input_to_lists(input);
+    count(levels, is_safe)
+}
+
+pub fn part_two(input: &str) -> usize {
+    let levels = read_input_to_lists(input);
+    count(levels, is_safe_part_two)
+}
+
+fn read_input_to_lists(input: &str) -> Vec<Vec<i64>> {
+    input
         .lines()
         .map(|line| {
             line.split_whitespace()
                 .map(|num| num.parse().unwrap())
                 .collect()
         })
-        .collect();
-
-    levels.iter().filter(|level| is_safe(level)).count()
+        .collect()
 }
 
-pub fn part_two(input: &str) -> i64 {
-    0
+fn count(levels: Vec<Vec<i64>>, predicate: impl Fn(&[i64]) -> bool) -> usize {
+    levels.iter().filter(|level| predicate(level)).count()
+}
+
+fn is_valid_sequence(level: &[i64], get_diff: impl Fn(&i64, &i64) -> i64) -> bool {
+    level
+        .iter()
+        .tuple_windows()
+        .all(|(a, b)| (1..=3).contains(&get_diff(a, b)))
 }
 
 fn is_safe(level: &[i64]) -> bool {
-    let all_increasing = level
-        .iter()
-        .tuple_windows::<(&i64, &i64)>()
-        .all(|(a, b)| a - b <= 3 && a - b > -0);
+    is_valid_sequence(level, |a, b| a - b) || is_valid_sequence(level, |a, b| b - a)
+}
 
-    let all_decreasing = level
-        .iter()
-        .tuple_windows::<(&i64, &i64)>()
-        .all(|(a, b)| b - a <= 3 && b - a > 0);
-
-    all_increasing || all_decreasing
+fn is_safe_part_two(level: &[i64]) -> bool {
+    (0..level.len()).any(|i| {
+        let mut level = level.to_vec();
+        level.remove(i);
+        is_safe(&level)
+    })
 }
 
 #[test]
@@ -45,11 +58,12 @@ fn example_input_part1() {
 
 #[test]
 fn example_input_part2() {
-    let input = r#"3   4
-4   3
-2   5
-1   3
-3   9
-3   3"#;
-    assert_eq!(part_two(input), 31);
+    let input = r#"7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+"#;
+    assert_eq!(part_two(input), 4);
 }
