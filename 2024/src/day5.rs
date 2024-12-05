bf74ebcc218e0b595cb7af2d9be67bf965fd0cad
Geir Olav Alsvik <geir.olav.alsvik@sparebank1.no>
2024-12-05 13:46:43 +0100

    day 5 done

--- 2024/src/day5.rs
+++ 2024/src/day5.rs
@@ -1,8 +1,45 @@
-use std::collections::HashMap;
+use std::{
+    cmp::Ordering::{Greater, Less},
+    collections::HashMap,
+};
+
+use itertools::Itertools;
 
 pub fn part_one(input: &str) -> i64 {
     let (rules_str, updates_str) = input.split_once("\n\n").unwrap();
+    let rule_map = build_rule_map(rules_str);
 
+    updates_str
+        .lines()
+        .filter(|update| is_valid_update(update, &rule_map))
+        .map(get_middle_number)
+        .sum()
+}
+
+pub fn part_two(input: &str) -> i64 {
+    let (rules_str, updates_str) = input.split_once("\n\n").unwrap();
+    let rule_map = build_rule_map(rules_str);
+
+    updates_str
+        .lines()
+        .filter(|update| !is_valid_update(update, &rule_map))
+        .map(|update| {
+            update
+                .split(',')
+                .sorted_by(|a, b| {
+                    if rule_map.get(a).unwrap().contains(b) {
+                        Greater
+                    } else {
+                        Less
+                    }
+                })
+                .join(",")
+        })
+        .map(|update| get_middle_number(&update))
+        .sum()
+}
+
+fn build_rule_map(rules_str: &str) -> HashMap<&str, Vec<&str>> {
     let mut rule_map: HashMap<&str, Vec<&str>> = HashMap::new();
 
     for rule in rules_str.lines() {
@@ -12,12 +49,21 @@ pub fn part_one(input: &str) -> i64 {
             .and_modify(|el| el.push(value))
             .or_insert(vec![value]);
     }
+    rule_map
+}
 
-    let valid_updates = updates_str
-        .lines()
-        .filter(|update| is_valid_update(update, &rule_map));
-
-    valid_updates.map(get_middle_number).sum()
+fn is_valid_update(update: &str, rule_map: &HashMap<&str, Vec<&str>>) -> bool {
+    let numbers = update.split(',').collect::<Vec<&str>>();
+    let mut printed = vec![];
+    for (i, number) in numbers.iter().enumerate() {
+        let rest = &numbers[i..];
+        let rules = rule_map.get(number).unwrap();
+        if rules.iter().any(|el| rest.contains(el)) {
+            return false;
+        }
+        printed.push(number);
+    }
+    true
 }
 
 fn get_middle_number(numbers: &str) -> i64 {
@@ -28,28 +74,10 @@ fn get_middle_number(numbers: &str) -> i64 {
     numbers_i64[(numbers_i64.len() - 1) / 2]
 }
 
-fn is_valid_update(update: &str, rule_map: &HashMap<&str, Vec<&str>>) -> bool {
-    let numbers = update.split(',').collect::<Vec<&str>>();
-    let mut printed = vec![];
-    for (i, number) in update.split(',').enumerate() {
-        let rest = &numbers[i..];
-        if let Some(rules) = rule_map.get(number) {
-            if rules.iter().any(|el| rest.contains(el)) {
-                return false;
-            }
-        }
-        printed.push(number);
-    }
-    true
-}
+mod tests {
+    use super::*;
 
-pub fn part_two(input: &str) -> i64 {
-    0
-}
-
-#[test]
-fn test_part_one() {
-    let input = r#"47|53
+    const SAMPLE_INPUT: &str = r#"47|53
 97|13
 97|61
 97|47
@@ -77,5 +105,48 @@ fn test_part_one() {
 75,97,47,61,53
 61,13,29
 97,13,75,29,47"#;
-    assert_eq!(part_one(input), 143);
+
+    // #[test]
+    // fn test_part_one() {
+    //     assert_eq!(part_one(SAMPLE_INPUT), 143);
+    // }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two(SAMPLE_INPUT), 123);
+    }
 }
+
+// "75": [
+//     "97",
+// ],
+// "13": [
+//     "97",
+//     "61",
+//     "29",
+//     "47",
+//     "75",
+//     "53",
+// ],
+// "53": [
+//     "47",
+//     "75",
+//     "61",
+//     "97",
+// ],
+// "29": [
+//     "75",
+//     "97",
+//     "53",
+//     "61",
+//     "47",
+// ],
+// "61": [
+//     "97",
+//     "47",
+//     "75",
+// ],
+// "47": [
+//     "97",
+//     "75",
+// ],
