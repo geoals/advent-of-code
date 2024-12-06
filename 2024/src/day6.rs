2c506e49cb9be598ee19da1012b9b777bbd38c3a
Geir Olav Alsvik <goa95@protonmail.com>
2024-12-06 23:27:42 +0100

    day 6 cleanup

--- 2024/src/day6.rs
+++ 2024/src/day6.rs
@@ -1,163 +1,106 @@
-use std::{collections::HashSet, str::FromStr};
-
-use Direction::*;
-use Tile::*;
+use std::collections::HashSet;
 
 pub fn part_one(input: &str) -> usize {
-    let map: Vec<Vec<Tile>> = input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|c| match c {
-                    '#' => Tile::Obstacle,
-                    '.' | '<' | '>' | '^' | 'V' => Tile::Traversable,
-                    _ => panic!("Invalid character"),
-                })
-                .collect()
-        })
-        .collect();
+    let map = build_map(input);
+    let (position, direction) = start_pos_and_direction(&map);
+    let visited = traverse_map(&map, position, direction).unwrap();
+    visited.len()
+}
 
-    let mut starting_position = (0, 0);
+pub fn part_two(input: &str) -> usize {
+    let map = build_map(input);
+    let (position, direction) = start_pos_and_direction(&map);
+    let visited = traverse_map(&map, position, direction).unwrap();
 
-    for (y, row) in input.lines().enumerate() {
-        for (x, tile) in row.chars().enumerate() {
-            if ['<', '>', 'V', '^'].contains(&tile) {
-                starting_position = (x, y);
-                break;
+    let mut cycle_count = 0;
+    for (x, y) in visited {
+        let mut new_map = map.clone();
+        new_map[y][x] = '#';
+        if traverse_map(&new_map, position, direction).is_none() {
+            cycle_count += 1;
+        }
+    }
+    cycle_count
+}
+
+/// Returns none if a cycle is detected
+pub fn traverse_map(
+    map: &[Vec<char>],
+    mut position: (usize, usize),
+    mut direction: char,
+) -> Option<HashSet<(usize, usize)>> {
+    let mut visited = HashSet::from([position]);
+    let mut visited_with_direction = HashSet::new();
+
+    while inside_bounds(map, &position, &direction) {
+        if is_blocked(map, &position, &direction) {
+            turn_right(&mut direction);
+        }
+        move_forward(&mut position, &direction);
+
+        if visited_with_direction.contains(&(position, direction)) {
+            return None;
+        }
+
+        visited.insert(position);
+        visited_with_direction.insert((position, direction));
+    }
+
+    Some(visited)
+}
+
+fn build_map(input: &str) -> Vec<Vec<char>> {
+    input.lines().map(|line| line.chars().collect()).collect()
+}
+
+fn start_pos_and_direction(input: &[Vec<char>]) -> ((usize, usize), char) {
+    for (y, row) in input.iter().enumerate() {
+        for (x, tile) in row.iter().enumerate() {
+            if ['<', '>', 'V', '^'].contains(tile) {
+                return ((x, y), *tile);
             }
         }
     }
-
-    let mut guard = Guard {
-        x: starting_position.0 as i64,
-        y: starting_position.1 as i64,
-        direction: Direction::from_str(
-            &input
-                .chars()
-                .find(|c| ['<', '>', 'V', '^'].contains(c))
-                .unwrap()
-                .to_string(),
-        )
-        .unwrap(),
-        finished_moving: false,
-        visited: HashSet::from([(starting_position.1 as i64, starting_position.0 as i64)]),
-    };
-
-    while !guard.finished_moving {
-        guard.move_one_tile(&map);
-    }
-
-    guard.visited.len()
+    panic!("No starting position found");
 }
 
-pub fn part_two(input: &str) -> i64 {
-    0
-}
-
-fn out_of_bounds(x: i64, y: i64, map: &[Vec<Tile>]) -> bool {
-    x >= map[0].len() as i64 || y >= map.len() as i64 || x < 0 || y < 0
-}
-
-#[derive(Debug)]
-struct Guard {
-    x: i64,
-    y: i64,
-    direction: Direction,
-    visited: HashSet<(i64, i64)>,
-    finished_moving: bool,
-}
-
-impl Guard {
-    fn move_one_tile(&mut self, map: &[Vec<Tile>]) {
-        if self.is_blocked(map) {
-            self.turn_right();
-        }
-        match self.direction {
-            Left => self.x -= 1,
-            Right => self.x += 1,
-            Down => self.y += 1,
-            Up => self.y -= 1,
-        }
-
-        if out_of_bounds(self.x, self.y, map) {
-            self.finished_moving = true;
-            return;
-        }
-
-        self.visited.insert((self.y, self.x));
-    }
-
-    // blocked if tile in front is not traversable
-    fn is_blocked(&self, map: &[Vec<Tile>]) -> bool {
-        let next_x = match self.direction {
-            Left => {
-                if self.x == 0 {
-                    return false;
-                }
-                self.x - 1
-            }
-            Right => {
-                if self.x as usize >= map[0].len() - 1 {
-                    return false;
-                }
-                self.x + 1
-            }
-            _ => self.x,
-        };
-
-        let next_y = match self.direction {
-            Up => {
-                if self.y == 0 {
-                    return false;
-                }
-                self.y - 1
-            }
-            Down => {
-                if self.y as usize >= map.len() - 1 {
-                    return false;
-                }
-                self.y + 1
-            }
-            _ => self.y,
-        };
-
-        map[next_y as usize][next_x as usize] == Obstacle
-    }
-
-    fn turn_right(&mut self) {
-        self.direction = match self.direction {
-            Left => Up,
-            Right => Down,
-            Down => Left,
-            Up => Right,
-        }
+fn inside_bounds(map: &[Vec<char>], position: &(usize, usize), direction: &char) -> bool {
+    match direction {
+        '<' => position.0 > 0,
+        '>' => position.0 < map[0].len() - 1,
+        '^' => position.1 > 0,
+        'V' => position.1 < map.len() - 1,
+        _ => panic!("Invalid direction"),
     }
 }
 
-#[derive(Debug)]
-enum Direction {
-    Left,
-    Right,
-    Down,
-    Up,
+fn is_blocked(map: &[Vec<char>], position: &(usize, usize), direction: &char) -> bool {
+    match direction {
+        '<' => map[position.1][position.0 - 1] == '#',
+        '>' => map[position.1][position.0 + 1] == '#',
+        '^' => map[position.1 - 1][position.0] == '#',
+        'V' => map[position.1 + 1][position.0] == '#',
+        _ => panic!("Invalid direction"),
+    }
 }
 
-#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
-enum Tile {
-    Obstacle,
-    Traversable,
+fn turn_right(direction: &mut char) {
+    match direction {
+        '<' => *direction = '^',
+        '^' => *direction = '>',
+        '>' => *direction = 'V',
+        'V' => *direction = '<',
+        _ => panic!("Invalid direction"),
+    }
 }
 
-impl FromStr for Direction {
-    type Err = ();
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "<" => Ok(Left),
-            ">" => Ok(Right),
-            "V" => Ok(Down),
-            "^" => Ok(Up),
-            _ => Err(()),
-        }
+fn move_forward(position: &mut (usize, usize), direction: &char) {
+    match direction {
+        '<' => position.0 -= 1,
+        '>' => position.0 += 1,
+        '^' => position.1 -= 1,
+        'V' => position.1 += 1,
+        _ => panic!("Invalid direction"),
     }
 }
 
@@ -175,4 +118,20 @@ fn test_part_one() {
 ......#..."#;
 
     assert_eq!(part_one(input), 41);
+}
+
+#[test]
+fn test_part_two() {
+    let input = r#"....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#..."#;
+
+    assert_eq!(part_two(input), 6);
 }
